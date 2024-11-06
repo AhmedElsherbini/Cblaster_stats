7d6184b7ec946f863011f9e5ace56d07d2159d7a
Ahmed M. A. Elsherbini <drahmedelsherbini@gmail.com>
2024-11-06 18:33:17 +0100

    Update cblaster_stats.py

--- cblaster_stats.py
+++ cblaster_stats.py
@@ -3,9 +3,8 @@
 Original author : Ahmed Elsherbini 
 Date 22-03-2024
 Spyder Editor
-
-This is a temporary script file.
 """
+###########################################################
 
 import pandas as pd
 from Bio import Entrez
@@ -15,6 +14,7 @@ from ete3 import Tree
 from ete3 import PhyloTree
 import argparse
 import warnings
+from Bio import Phylo
 ###########################################################
 #/media/ahmed/Elements1/old_projects/20_jens_je2
 warnings.filterwarnings("ignore")
@@ -24,58 +24,75 @@ my_parser.add_argument('-i','--input',
                        metavar='input',
                        type=str,
                        help='the path to your file')
+
+my_parser.add_argument('-ot','--outlier',
+                       action='store',
+                       metavar='outlier',
+                       type=str,
+                       help='the name to your outlier species')
+
 ###########################################################
 
 args = my_parser.parse_args()
 f_name = args.input
+ot = args.outlier
+
+#f_name = "example_binary.csv"
+#ot = "deinococcus_radiodurans"
 ###########################################################
-Entrez.email = 'drahmed@gmail.com'
-df = pd.read_csv(f_name,header=None)
-#df = pd.read_csv('30_binary.csv',header=None)
-df = df.iloc[:, 0].to_frame()
-df = df[0].str.split().str[:2].str.join(' ').to_frame()
-df = df[0].value_counts().to_frame()
-df = df.reset_index()		
-assmebly = []
-print("This is Cblaster_stats (Ahmed Elsherbini)")
-for row in df[0]:
-        species_name = str(row)
-        handle = Entrez.esearch(db="assembly", term=str(species_name), retmode="xml")
-        record = Entrez.read(handle)
-        count = int(record['Count'])
-        print(f"Number of {species_name} occurrences in NCBI assembly database: {count}")
-        assmebly.append(count)
-
-df['assmebly'] = assmebly
-
-df['%_in_assembly_db'] = df['count']/df['assmebly']*100
-df = df.rename(columns={0: 'Species'})
-file_name = 'database percentage_%s.csv'%(f_name[:-4])
-df.to_csv(file_name,index=False)
-print("Done for the database file")
-
-
 try:
+    Entrez.email = 'drahmed@gmail.com'
+    df = pd.read_csv(f_name,header=None)
+    #df = pd.read_csv('30_binary.csv',header=None)
+    df = df.iloc[:, 0].to_frame()
+    df = df[0].str.split().str[:2].str.join(' ').to_frame()
+    df = df[0].value_counts().to_frame()
+    df = df.reset_index()		
+    assmebly = []
+    print("This is Cblaster_stats (Ahmed Elsherbini)")
+    for row in df[0]:
+            species_name = str(row)
+            handle = Entrez.esearch(db="assembly", term=str(species_name), retmode="xml")
+            record = Entrez.read(handle)
+            count = int(record['Count'])
+            print(f"Number of {species_name} occurrences in NCBI assembly database: {count}")
+            assmebly.append(count)
+    
+    df['assmebly'] = assmebly
+    
+    df['%_in_assembly_db'] = df['count']/df['assmebly']*100
+    df = df.rename(columns={0: 'Species'})
+    file_name = 'database percentage_%s.csv'%(f_name[:-4])
+    df.to_csv(file_name,index=False)
+    print("Done for the database file")
+    
+    
+    #try:
     ncbi = NCBITaxa()
     species_taxids = {}
-    species_names = df["Species"]
+    species_names = pd.Series(df["Species"])
+    #add the outlier
+    if ot == True:
+        ot = ot.replace("_", " ")
+        species_names.loc[len(species_names)] = str(ot)
+    
     for species_name in species_names:
         # Use the ETE Toolkit to search for the taxid of the given species name
         taxid = ncbi.get_name_translator([species_name])
-        # Check if the species name exists in the NCBI Taxonomy database
+        # Check if the species name exists in the NCBI Taxonomy database   
         if taxid:
             species_taxids[species_name] = taxid[species_name][0]
         else:
             species_taxids[species_name] = None
-    
-    
+       
+       
     taxa_ids = list(species_taxids.values())
     taxa_ids = [i for i in taxa_ids if i is not None]
     tree = ncbi.get_topology(taxa_ids)
     #just to print in Python API
     #tree = tree.get_ascii(attributes=["sci_name"])
-    
-    
+       
+       
     def annotate_tree_with_scientific_names(tree):
         for node in tree.traverse():
             if node.is_leaf():
@@ -85,15 +102,20 @@ try:
                 # Update node name with scientific name
                 node.name = scientific_name if scientific_name else "Unknown"
     
-    
+       
     
     annotate_tree_with_scientific_names(tree)
     
     output_file = "%s_tree.nwk"%(f_name[:-4])
-    
     # Export the annotated tree as a Newick file
+    #the tree
     tree.write(outfile=output_file)
-    tree.render("%s_tree.png"%(f_name[:-4]), layout=None, w=None, h=None, tree_style=None, units='px', dpi=90) 
-    print("We are done here, check the tree!")
+    tree.render("%s_tree.pdf"%(f_name[:-4]), layout=None, w=None, h=None, tree_style=None, units='px', dpi=90) 
+
+    print("We are done for free, check the tree!")
+    #here we 
+    df1 = df[['Species', '%_in_assembly_db']]
+    
+
 except:
- print("You may have an error with ete3 installation")
+    print("You may have an error with ete3 installation")
